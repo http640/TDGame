1090eeb7809b54b58b28a5b2b85fa5577b5f4dc1
Saman Ashrafzadeh Nik <http640@gmail.com>
2019-08-10 13:57:53 +0430

    GameManager Script
    GameManager:
    MaxEnemiesOnScreen - Enemies[]- totalEnemies- EnemiesPerSpawn- spawnPoint

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+    public GameObject spawnPoint;
+    public GameObject[] enemies;
+    public int totalEnemies;
+    public int maxEnemiesOnScreen;
+    public int enemiesPerSpawn;
+
+    private int enemiesOnScreen = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnEnemy();   
+    }
+
+    void SpawnEnemy()
+    {
+        if (enemiesPerSpawn > 0 && enemiesOnScreen < maxEnemiesOnScreen)
+        {
+            for (int i=0; i<enemiesPerSpawn; ++i)
+            {
+                if (enemiesOnScreen < maxEnemiesOnScreen)
+                    for (int j = enemiesOnScreen; j < maxEnemiesOnScreen; ++j)
+                    {
+                        GameObject newEnemy = Instantiate(enemies[1]) as GameObject;
+                        newEnemy.transform.position = spawnPoint.transform.position;
+                        enemiesOnScreen++;
+                    }
+
+            }
+        }
     }
 
     // Update is called once per frame
